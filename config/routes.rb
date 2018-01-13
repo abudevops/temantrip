a9200aa6f7c1b220f056632c2ad00248eaf12ea9
abudevops <enal.ty@gmail.com>
2018-01-14 00:36:03 +0700

    Page Detail

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'umroh/list', to: 'umroh#list'
+  get 'umroh/detail', to: 'umroh#detail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
